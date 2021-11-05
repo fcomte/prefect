b1d6034c920e5aa500ff0b1987055811e11797d1
fcomte <48868412+fcomte@users.noreply.github.com>
2021-11-05 17:23:42 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
-import prefect
-from prefect import task, Flow
+from prefect import task, Flow, Parameter
+from prefect.storage import GitHub
 
-@task
-def hello_task():
-    logger = prefect.context.get("logger")
-    logger.info("Hello world!")
+@task(log_stdout=True)
+def say_hello(name):
+    print("Hello, {}!".format(name))
 
-with Flow("hello-flow") as flow:
-    hello_task()
+
+with Flow("Mon flow github") as flow:
+    name = Parameter('name')
+    say_hello(name)
+
+flow.storage = GitHub(repo="fcomte/prefect", path="test.py")
